ea512c768cf4d6710c099da5815c2172fca96077
alandrnov <al.andr.nov@gmail.com>
2021-12-09 09:33:11 +0300

    OrderService Test updated

--- src/test/java/com/amr/project/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/amr/project/service/impl/OrderServiceImplTest.java
@@ -1,20 +1,16 @@
 package com.amr.project.service.impl;
 
 import com.amr.project.converter.ItemMapper;
-import com.amr.project.converter.OrderMapper;
-
 import com.amr.project.model.dto.ItemDto;
-
+import com.amr.project.model.dto.OrderDto;
 import com.amr.project.model.entity.Item;
 import com.amr.project.model.entity.Order;
 import com.amr.project.model.entity.User;
 import com.amr.project.service.abstracts.ItemService;
 import com.amr.project.service.abstracts.UserService;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -42,26 +38,20 @@ public class OrderServiceImplTest {
     ItemService itemService;
 
     User user = new User();
+    List<ItemDto> items = new ArrayList<>();
 
-//    @Mock
-//    private OrderMapper orderMapper;
+    OrderDto orderDto = new OrderDto();
 
     @Before
-    public void createUser(){
+    public void createUserAndItem() {
 
         user.setUsername("Arseniy");
         user.setPassword("8800-");
         user.setEmail("as@mail.com");
-        user.setPhone("99999999999");
+        user.setPhone("77777777777");
         user.setBirthday(new Calendar.Builder().setDate(2001, 3, 15).build());
         userService.persist(user);
 
-    }
-
-
-    @Test
-    public void collectOrder(){
-        List<ItemDto> items = new ArrayList<>();
         Item item = Item.builder()
                 .name("Маска медицинская")
                 .basePrice(BigDecimal.valueOf(10))
@@ -76,21 +66,44 @@ public class OrderServiceImplTest {
         itemService.persist(item);
         items.add(itemMapper.itemToItemDto(item));
 
-        Order order = orderService.collectOrderByUserAndItems(items,user);
+    }
 
-        assertEquals("as@mail.com", user.getEmail());
-        assertEquals("99999999999", user.getPhone());
+
+    @Test
+    public void collectOrder() {
+
+        Order order = orderService.collectOrderByUserAndItems(items, user);
+
+
+        assertEquals("as@mail.com", order.getUser().getEmail());
+        assertEquals("77777777777", order.getBuyerPhone());
         assertNotNull(order.getStatus());
         assertNotNull(order.getTotal());
         assertNotNull(order.getId());
+    }
 
 
+    @Test
+    public void updateAddressAndUInfo() {
+        Order order = orderService.collectOrderByUserAndItems(items, user);
+
+        orderDto.setBuyerPhone("10987654321");
+        orderDto.setBuyerName("Petya");
+
+
+        orderService.updateAddressAndUserInfo(1L, orderDto);
+
+        assertEquals("10987654321", orderService.getByKey(1L).getBuyerPhone());
+        assertEquals("Petya", orderService.getByKey(1L).getBuyerName());
 
     }
 
     @Test
-    public void DeleteItem(){
-        orderService.deleteItemInOrder(1L,1L);
+    public void DeleteItem() {
+        Order order = orderService.collectOrderByUserAndItems(items, user);
+        orderService.deleteItemInOrder(1L, 1L);
+
+        assertFalse(order.getItems().isEmpty());
 
     }
 }
