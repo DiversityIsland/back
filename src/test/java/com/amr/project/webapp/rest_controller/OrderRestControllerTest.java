44fe39c6cb10a40f8382fb8f5fbf90f38fc46c44
Roman <79163288+besSmertniyKoder@users.noreply.github.com>
2021-12-13 00:13:58 +0300

    Написал тесты

--- src/test/java/com/amr/project/webapp/rest_controller/OrderRestControllerTest.java
+++ src/test/java/com/amr/project/webapp/rest_controller/OrderRestControllerTest.java
@@ -1,35 +1,55 @@
 package com.amr.project.webapp.rest_controller;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class OrderRestControllerTest {
 
-//    @BeforeEach
-//    void setUp() {
-//    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
 
     @Test
-    void setStatusStart() {
+    void setStatusStart() throws Exception {
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/order/start/1");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
     }
 
     @Test
-    void setStatusComplete() {
+    void setStatusComplete() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/order/complete/1");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
     }
 
     @Test
-    void setStatusSent() {
+    void setStatusSent() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/order/sent/1");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
     }
 
     @Test
-    void setStatusDelivered() {
+    void setStatusDelivered() throws  Exception{
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/order/sent/1");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
     }
 }
