f136f477b6bc974aeec55db813202f1921025715
Kulakov Pavel <kombojiec@gmail.com>
2022-01-24 22:15:58 +0300

    jwt(feat): create main jwt functionality for server side

---
+++ src/main/java/com/amr/project/jwt/jwt_realization/AuthService.java
@@ -0,0 +1,69 @@
+package com.amr.project.jwt.jwt_realization;
+
+import com.amr.project.jwt.model.JwtRequest;
+import com.amr.project.jwt.model.JwtResponse;
+import com.amr.project.model.entity.User;
+import com.amr.project.service.abstracts.UserService;
+import io.jsonwebtoken.Claims;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.message.AuthException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserService userService;
+    private final Map<String, String> refreshStorage = new HashMap<>();
+    private final JwtProvider jwtProvider;
+
+    public JwtResponse login(@NonNull JwtRequest authRequest) throws AuthException {
+        final User user = userService.findByUsername(authRequest.getLogin())
+                .orElseThrow(() -> new AuthException("Пользователь не найден"));
+        if (user.getPassword().equals(authRequest.getPassword())) {
+            final String accessToken = jwtProvider.generateAccessToken(user);
+            final String refreshToken = jwtProvider.generateRefreshToken(user);
+            refreshStorage.put(user.getUsername(), refreshToken);
+            return new JwtResponse(accessToken, refreshToken, user.getUsername(), user.getRoles());
+        } else {
+            throw new AuthException("Неправильный пароль");
+        }
+    }
+
+    public JwtResponse getAccessToken(@NonNull String refreshToken) throws AuthException {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final String login = claims.getSubject();
+            final String saveRefreshToken = refreshStorage.get(login);
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final User user = userService.findByUsername(login)
+                        .orElseThrow(() -> new AuthException("Пользователь не найден"));
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                return new JwtResponse(accessToken, null, user.getUsername(), user.getRoles());
+            }
+        }
+        return new JwtResponse(null, null, null, null);
+    }
+
+    public JwtResponse refresh(@NonNull String refreshToken) throws AuthException {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final String login = claims.getSubject();
+            final String saveRefreshToken = refreshStorage.get(login);
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final User user = userService.findByUsername(login)
+                        .orElseThrow(() -> new AuthException("Пользователь не найден"));
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                final String newRefreshToken = jwtProvider.generateRefreshToken(user);
+                refreshStorage.put(user.getUsername(), newRefreshToken);
+                return new JwtResponse(accessToken, newRefreshToken, user.getUsername(), user.getRoles());
+            }
+        }
+        throw new AuthException("Невалидный JWT токен");
+    }
+
+}
---
+++ src/main/java/com/amr/project/jwt/model/RefreshJwtRequest.java
@@ -0,0 +1,12 @@
+package com.amr.project.jwt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RefreshJwtRequest {
+
+    public String refreshToken;
+
+}
